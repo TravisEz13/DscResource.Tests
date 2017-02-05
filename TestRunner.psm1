f8973c05bfc544fb4041d72da02b270600d290dd
Travis Plunk <travis.plunk@microsoft.com>
2017-02-04 16:55:20 -0800

    Add function to invoke dsc resource tests

--- TestRunner.psm1
+++ TestRunner.psm1
@@ -39,4 +39,127 @@ function Start-DscResourceTests
     Pop-Location
 }
 
-Export-ModuleMember -Function @( 'Start-DscResourceTests' )
+<#
+    .SYNOPSIS
+        Runs Pester tests and reports results
+
+    .PARAMETER Path
+        The Path to the tests to run
+
+    .PARAMETER CodeCoverage
+        An array of paths to files to run CodeCoverage for.
+
+    .EXAMPLE
+        Invoke-DscResourceTests -Path C:\myModule\tests -CodeCoverage @('C:\myModule\myModule.psm1')
+
+#>
+function Invoke-DscResourceTests {
+    [CmdletBinding()]
+    param
+    (
+        [CmdletBinding()]
+        [string]
+        $Path, 
+        
+        [Object[]] 
+        $CodeCoverage
+    )
+
+    Write-Info "Running tests: $Path"
+    $testResultPath = 'TestsResults.xml'
+    
+    $results = Invoke-Pester -OutputFormat NUnitXml -OutputFile $testResultPath -PassThru @PSBoundParameters
+
+    if(${env:APPVEYOR_JOB_ID})
+    {
+        foreach($result in $results.TestResult)
+        {
+            [string] $describeName = $result.Describe -replace '\\', '/'
+            [string] $contextName = $result.Context -replace '\\', '/'
+            $componentName = '{0}; Context: {1}' -f $describeName, $contextName
+
+            #Write-Info ('Adding test result {0} - {1}, Outcome: {2}, Duration {3}' -f $componentName, $result.Name, $result.Result, $result.Time.TotalMilliseconds)
+            Add-AppveyorTest -Name $result.Name -Framework NUnit -Filename $componentName -Outcome $result.Result -Duration $result.Time.TotalMilliseconds
+        }
+    }
+    else {
+        Write-Info 'Not in AppVeyor.  Skipping uploading test results.'
+    }
+
+
+    Push-TestArtifact -Path $testResultPath
+
+    Write-Info 'Done running tests.'
+    Write-Info "Test result Type: $($results.gettype().fullname)"
+    
+    if ($results.FailedCount -gt 0) 
+    { 
+                throw "$($results.FailedCount) tests failed."    
+    }
+
+    return $results
+}
+
+<#
+    .SYNOPSIS
+        Writes information to the build log
+
+    .PARAMETER Message
+        The Message to write
+
+    .EXAMPLE
+        Write-Info -Message "Some build info"
+
+#>
+function Write-Info {
+    [CmdletBinding()]
+     param
+     (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string]
+         $Message
+     )
+
+    Write-Host -ForegroundColor Yellow  "[Build Info] [$([datetime]::UtcNow)] $message"
+}
+
+<#
+    .SYNOPSIS
+        Uploads test artifacts
+
+    .PARAMETER Path
+        The path to the test artifacts
+
+    .EXAMPLE
+        Push-TestArtifact -Path .\TestArtifact.log
+
+#>
+function Push-TestArtifact
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]
+        $Path
+    )    
+
+    $resolvedPath = (Resolve-Path $Path).ProviderPath
+    if(${env:APPVEYOR_JOB_ID})
+    {
+        <# does not work with Pester 4.0.2
+        $url = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
+        Write-Info -Message "Uploading Test Results: $resolvedPath ; to: $url"
+        (New-Object 'System.Net.WebClient').UploadFile($url, $resolvedPath)
+        #>
+
+        Write-Info -Message "Uploading Test Artifact: $resolvedPath"
+        Push-AppveyorArtifact $resolvedPath
+    }
+    else
+    {
+        Write-Info -Message "Test Artifact: $resolvedPath"
+    }
+}
+
+Export-ModuleMember -Function @( 'Start-DscResourceTests','Invoke-DscResourceTests' )
